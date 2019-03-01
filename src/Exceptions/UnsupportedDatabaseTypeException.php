7864c7a227eca9cf3c0c91325425be13adfd6f37
scliftas <scliftas@gmail.com>
2019-03-01 23:36:36 +0000

    Added unsupported database type exception class

---
+++ src/Exceptions/UnsupportedDatabaseTypeException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Shaunclift\Flex\Exceptions;
+
+use Exception;
+
+class UnsupportedDatabaseTypeException extends Exception
+{
+    public function reporrt()
+    {
+
+    }
+
+    public function render($request)
+    {
+        
+    }
+}
