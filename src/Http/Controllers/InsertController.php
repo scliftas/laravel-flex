44b18e9cfd3ff3e4619b4b364551e2dc3bcab8e3
scliftas <scliftas@gmail.com>
2019-03-03 17:56:22 +0000

    [WIP] Added insert controller

--- src/Http/Controllers/InsertController.php
+++ src/Http/Controllers/InsertController.php
@@ -2,10 +2,13 @@
 
 namespace Shaunclift\Flex\Http\Controllers;
 
+use Shaunclift\Flex\Managers\DatabaseManager;
+
 class InsertController extends AbstractController
 {
     public function index()
     {
-        echo "Hello world!";
+        $manager = new DatabaseManager('mysql');
+        dd($manager->create('test_db5'));
     }
 }
